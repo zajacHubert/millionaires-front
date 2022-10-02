a2adb13396c1a6cf6dd296eba69b1ebe31bc06fb
Hubert <hubert.zajac92@gmail.com>
2022-10-02 16:08:26 +0200

    Fix basic url

--- src/utils/axios-functions.ts
+++ src/utils/axios-functions.ts
@@ -1,11 +1,10 @@
 import axios from "axios";
 
 const isDev = process.env.NODE_ENV === 'development';
-const baseUrl = isDev ? 'http://localhost:3001' : 'https://millionaires-hubert.herokuapp.com';
-
+const baseUrl = isDev ? 'http://localhost:3001/questions' : 'https://millionaires-hubert.herokuapp.com/questions';
 
 export const getAllQuestions = async () => {
-    const res = await axios.get(`${baseUrl}/questions`);
+    const res = await axios.get(baseUrl);
     return res;
 }
 
