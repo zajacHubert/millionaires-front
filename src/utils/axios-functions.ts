7aefba83177c22723dcb8af6f40e2f819e6c46d2
Hubert <hubert.zajac92@gmail.com>
2022-10-02 15:54:39 +0200

    Change url for prod

--- src/utils/axios-functions.ts
+++ src/utils/axios-functions.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const isDev = process.env.NODE_ENV === 'development';
-const baseUrl = isDev ? 'http://localhost:3001' : 'https://millionaires-hubert.herokuapp.com/';
+const baseUrl = isDev ? 'http://localhost:3001' : 'https://millionaires-hubert.herokuapp.com';
 
 
 export const getAllQuestions = async () => {
